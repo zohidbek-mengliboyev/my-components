f9a68d219aa4950cdb276592ec92b643e8327293
zohidbek-mengliboyev <uzrivojcoder@gmail.com>
2023-03-12 23:56:25 +0500

    Execute the following Angular CLI command to create an Angular component

---
+++ projects/ui-controls/src/lib/card-list/card-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-card-list',
+  templateUrl: './card-list.component.html',
+  styleUrls: ['./card-list.component.css']
+})
+export class CardListComponent {
+
+}
--- projects/ui-controls/src/lib/ui-controls.module.ts
+++ projects/ui-controls/src/lib/ui-controls.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { UiControlsComponent } from './ui-controls.component';
+import { CardListComponent } from './card-list/card-list.component';
 
 
 
 @NgModule({
   declarations: [
-    UiControlsComponent
+    UiControlsComponent,
+    CardListComponent
   ],
   imports: [
   ],
   exports: [
-    UiControlsComponent
+    UiControlsComponent,
+    CardListComponent
   ]
 })
 export class UiControlsModule { }
